e9c231a75f6531ff68013566f5b62d85e4887420
taras.leshchenko <taras.leshchenko@cs.ucalgary.ca>
2022-10-08 17:38:26 -0600

    input output, clear screen test keys A D to display key;  Z to exit

--- test_files/stubby.s
+++ test_files/stubby.s
@@ -1,7 +1,8 @@
     processor 6502
 
 ; KERNAL [sic] routines
-CHROUT = $ffd2
+CHROUT =    $ffd2
+CHRIN  =    $ffcf
 
     org     $1001
 
@@ -13,6 +14,48 @@ stubend
     dc.w    0
 
 start
-    lda     #'X
-    jsr     CHROUT
+
+    lda     #32
+    ldx     #0
+whitescreen
+
+    STA     $1e00,X
+    INX     
+
+    cpx     #255
+    BNE     whitescreen
+
+    lda     #01
+    ldx     #0
+
+
+loop
+    lda     $00C5
+    cmp     #64
+    beq     loop
+    cmp     #17 
+    beq     is_a
+    cmp     #18 
+    beq     is_d
+    cmp     #33
+    beq     exit_prg
+    jmp     loop
+
+is_a
+;    lda     #65 
+    lda     #01
+    jmp     printchar
+is_d
+;    lda     #68 
+    lda     #04
+    jmp     printchar
+
+printchar
+;    jsr     CHROUT
+    STA     $1e00
+    jmp     loop
+
+
+exit_prg
     rts
+    
